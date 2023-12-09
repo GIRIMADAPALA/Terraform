6e4bf06ab9169e853142aaecdb607f67887b5d9e
GIRIMADAPALA <121393030+GIRIMADAPALA@users.noreply.github.com>
2023-12-09 14:45:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 provider "aws" {
         region = "us-east-1"
+	access_key = "AKIA6DQI24FDJSPSL4PQ"
+	secret_key = "oBnAJSKPCXxEcQdKb4PrmdXj+/QCwMJH6LZUKiog"
 }
 
-resource "aws_s3_bucket" "example" {
-        bucket = "example-test1-s3"
+resource "aws_instance" "example" {
+        ami = "ami-06ca3ca175f37dd66"
+        instance_type = "t2.micro"
 }
+
