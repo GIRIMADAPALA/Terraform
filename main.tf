2c9d496a647281ec790dfdb25b0ff2960c8687c2
GIRIMADAPALA <121393030+GIRIMADAPALA@users.noreply.github.com>
2023-12-09 15:07:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 	access_key = "AKIA6DQI24FDJSPSL4PQ"
 	secret_key = "oBnAJSKPCXxEcQdKb4PrmdXj+/QCwMJH6LZUKiog"
 }
-
 resource "aws_instance" "example" {
-        ami = "ami-06ca3ca175f37dd66"
-        instance_type = "t2.micro"
+   ami = "ami-053b0d53c279acc90"
+   instance_type = "t2.micro"
+   key_name = "DevOps"
+   security_groups = ["DevOps"]
 }
-
