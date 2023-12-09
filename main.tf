6ed77efab9760738ca116bccc21a2a3c0542ddf7
GIRIMADAPALA <121393030+GIRIMADAPALA@users.noreply.github.com>
2023-12-09 13:31:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
         region = "us-east-1"
-	access_key = "AKIA6DQI24FDJSPSL4PQ"
-	secret_key = "oBnAJSKPCXxEcQdKb4PrmdXj+/QCwMJH6LZUKiog"
 }
 
 resource "aws_s3_bucket" "example" {
