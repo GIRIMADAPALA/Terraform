732fd8dba17c4080b4c29c3b0522578edca247e8
GIRIMADAPALA <121393030+GIRIMADAPALA@users.noreply.github.com>
2023-12-11 21:10:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
         region = "us-east-1"
-	access_key = "AKIA6DQI24FDJSPSL4PQ"
-	secret_key = "oBnAJSKPCXxEcQdKb4PrmdXj+/QCwMJH6LZUKiog"
 }
 resource "aws_instance" "example" {
    ami = "ami-053b0d53c279acc90"
